1993c8d1d8e5d2b41597058fc4d82aef2108daec
Aurélien Gâteau <mail@agateau.com>
2014-10-09 16:30:35 +0000

    Log output of check_cmd() command while it is running
    Should prevent buildbot from aborting the build because `make` runs for
    too long without outputing anything

--- outscale_image_factory/helper.py
+++ outscale_image_factory/helper.py
@@ -1,11 +1,22 @@
 """
 Helper functions used by the Outscale factory tools.
 """
-from __future__ import unicode_literals
+# Python 2/3 compatibility
+from __future__ import division, absolute_import, print_function, unicode_literals
 
 import logging
 import subprocess
 import os
+
+
+def _log_output(fp, prefix, lines):
+    line = fp.readline()
+    if not line:
+        return False
+    line = line.decode('utf-8', 'ignore')
+    logging.info(prefix + line.rstrip())
+    lines.append(line)
+    return True
 
 
 def check_cmd(cmd, data='', dryrun=False):
@@ -13,41 +24,45 @@ def check_cmd(cmd, data='', dryrun=False):
     success and debug data."""
     logging.info('Running {}'.format(repr(cmd)))
     if dryrun:
-        stderr = stdout = b''
+        stdout = b''
         ret = 0
     else:
-        popen = subprocess.Popen(cmd.split(' '), stdin=subprocess.PIPE,
-                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(cmd.split(' '),
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
         if data:
-            stdout, stderr = popen.communicate(data.encode('ascii'))
-        else:
-            stdout, stderr = popen.communicate()
-        ret = popen.wait()
-    error = dict(
+            proc.stdin.write(data.encode('ascii'))
+        proc.stdin.close()
+
+        # Run the command
+        lines = []
+        logging.info('pid: {}'.format(proc.pid))
+        prefix = '({}) '.format(proc.pid)
+        while proc.returncode is None:
+            proc.poll()
+            _log_output(proc.stdout, prefix, lines)
+
+        # Read the rest of proc.stdout
+        while _log_output(proc.stdout, prefix, lines):
+            pass
+        proc.stdout.close()
+        stdout = ''.join(lines)
+
+        ret = proc.returncode
+
+    data = dict(
         cmd=cmd,
         pwd=os.getcwd(),
         stdin=data,
-        stdout=stdout.decode('utf-8', 'ignore'),
-        stderr=stderr.decode('utf-8', 'ignore'),
-        ret=ret
+        stdout=stdout,
+        ret=ret,
     )
     ok = (ret == 0)
-    errstr = '''
-CMD={cmd}
-PWD={pwd}
-RET={ret}
-STDIN=
-{stdin}
-STDOUT=
-{stdout}
-STDERR=
-{stderr}
-'''.format(**error)
-    if ok:
-        logging.debug(errstr)
-    else:
-        logging.error(errstr)
-    return ok, error
+    if not ok:
+        logging.error('Command {} failed with error code {}'
+                      .format(repr(cmd), ret))
+    return ok, data
 
 
 def cd(path):
@@ -64,3 +79,18 @@ def cd(path):
     if not ok:
         logging.error(repr(err))
     return ok, err
+
+
+if __name__ == "__main__":
+    # Basic testing
+    logging.basicConfig(level=logging.INFO)
+    for args in [
+            ('find /etc/acpi',),
+            ('tr a-z A-Z', 'Some content'),
+            ('ls /does-not-exist',),
+            ]:
+        print('# Running {}'.format(args))
+        ok, data = check_cmd(*args)
+        print()
+        print('ok={}\ndata={}'.format(ok, data))
+        print()

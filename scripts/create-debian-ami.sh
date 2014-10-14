a50299cd7eb611ebf51486c0685e5375eb2d9631
Aurélien Gâteau <mail@agateau.com>
2014-10-14 17:42:01 +0200

    Rewrote script

--- scripts/create-debian-ami.sh
+++ scripts/create-debian-ami.sh
@@ -55,20 +55,25 @@ ARCH=$3
 DISTRO=$4
 MIRROR=$5
 
+# Urls
+GIT_DEBOOTSTRAP='git://anonscm.debian.org/d-i/debootstrap.git'
+GIT_MAKEDEV='git://git.gag.com/debian/makedev'
+GIT_TOOLS='git://github.com/nodalink/outscale-image-factory.git'
+MAKEDEV_VERSION=2.3.1
+MAKEDEV_URL="http://ftp.debian.org/debian/pool/main/m/makedev/makedev_${MAKEDEV_VERSION}.orig.tar.gz"
+
 # Dirs & paths
 BASEDIR=$(pwd)
-ROOTFS=${BASEDIR}/${DISTRO}
-DEBOOTSTRAP_DIR=$BASEDIR/debootstrap
-MAKEDEV=$BASEDIR/makedev/MAKEDEV
-PYTHONPATH=$BASEDIR/outscale-image-factory
-OVERLAY=$BASEDIR/outscale-image-factory/tklpatch/outscale/overlay
-export DEBOOTSTRAP_DIR PYTHONPATH
+SRCDIR=${BASEDIR}/src
+BUILDDIR=${BASEDIR}/build
+mkdir -p $SRCDIR $BUILDDIR
 
-# Gits
-GIT_DEBOOTSTRAP='git://anonscm.debian.org/d-i/debootstrap.git'
-#GIT_MAKEDEV='git://git.gag.com/debian/makedev'
-GIT_MAKEDEV='git://50.198.211.89/debian/makedev'
-GIT_TOOLS='git://github.com/nodalink/outscale-image-factory.git'
+ROOTFS=${BUILDDIR}/${DISTRO}
+DEBOOTSTRAP_DIR=$SRCDIR/debootstrap
+MAKEDEV=$SRCDIR/makedev-${MAKEDEV_VERSION}.orig/MAKEDEV
+PYTHONPATH=$SRCDIR/outscale-image-factory
+OVERLAY=$SRCDIR/outscale-image-factory/tklpatch/outscale/overlay
+export DEBOOTSTRAP_DIR PYTHONPATH
 
 # Chroot setup
 DEBIAN_KERNEL="linux-image-$ARCH"
@@ -77,6 +82,8 @@ LANG=C.UTF-8
 DEBIAN_FRONTEND=noninteractive
 export LANG DEBIAN_FRONTEND
 
+PACKAGES="openssh-server wget grub-pc"
+
 git_clone()
 {
     local repo=$1
@@ -84,82 +91,138 @@ git_clone()
     test -e $dir || git clone $repo $dir
 }
 
-echo "Checking for root"
+step()
+{
+    echo "=== $*"
+}
+
+get_python2_version()
+{
+    python2 <<EOF
+import sys
+print("%d.%d" % sys.version_info[:2])
+EOF
+}
+
+setup_centos_python()
+{
+    local version=$1
+    local python_root=/opt/rh/python${version}/root
+    if [ ! -d $python_root ] ; then
+        return
+    fi
+    step "Setting up paths for CentOS"
+    PATH=$python_root/usr/bin:$PATH
+    LD_LIBRARY_PATH=""
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$python_root/usr/lib
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$python_root/usr/lib64
+    export PATH LD_LIBRARY_PATH
+}
+
+missing_dependency()
+{
+    echo "Missing dependency: $1"
+    echo "For CentOS see: http://wiki.centos.org/AdditionalResources/Repositories/SCL"
+    exit 1
+}
+
+step "Checking for root"
 if [ "$(whoami)" != "root" ]
 then
     echo "Please run this script as root."
     exit 1
 fi
 
-echo "Checking for $DEVICE"
+step "Checking for $DEVICE"
 if [ ! -e "$DEVICE" ]
 then
     echo "Please attach a 10GB volume."
     exit 1
 fi
 
-echo "Setting up paths for CentOS"
-${LD_LIBRARY_PATH=:}
-for version in 34 33 32
-do
-    python_root=/opt/rh/python${version}/root
-    PATH=$python_root/usr/bin:$PATH
-    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$python_root/usr/lib
-    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$python_root/usr/lib64
-done
-export PATH LD_LIBRARY_PATH
+step "Checking Python2 version"
+if [ "$(get_python2_version)" != "2.7" ] ; then
+    setup_centos_python 27
+    if [ "$(get_python2_version)" != "2.7" ] ; then
+        missing_dependency "python 2.7"
+    fi
+fi
 
-echo "Checking for dependencies"
-for bin in wget git make rsync gpg parted python3
+step "Checking other dependencies"
+for bin in wget git make rsync gpg parted
 do
     if ! which $bin >/dev/null
     then
-        echo "Missing dependency: $bin"
-        echo "For CentOS see: http://wiki.centos.org/AdditionalResources/Repositories/SCL"
-        exit 1
+        missing_dependency "$bin"
     fi
 done
 
-echo "Cloning git repos"
-git_clone $GIT_MAKEDEV $BASEDIR/makedev
-git_clone $GIT_DEBOOTSTRAP $BASEDIR/debootstrap
-git_clone $GIT_TOOLS $BASEDIR/outscale-image-factory
-
-echo "Building debootstrap"
+step "Getting remote code"
+curl $MAKEDEV_URL | tar -C $SRCDIR -xz
+if [ ! -e $MAKEDEV ]
+then
+    echo "makedev tarball does not contain the MAKEDEV script"
+    exit 1
+fi
 chmod +x $MAKEDEV
+
+git_clone $GIT_DEBOOTSTRAP $SRCDIR/debootstrap
+git_clone $GIT_TOOLS $SRCDIR/outscale-image-factory
+
+step "Building debootstrap"
 make -C $DEBOOTSTRAP_DIR devices.tar.gz MAKEDEV=$MAKEDEV
 
-echo "Running debootstrap"
-test -e $ROOTFS || $DEBOOTSTRAP_DIR/debootstrap \
-    --verbose --keyring=$KEYRING --arch=$ARCH --include=openssh-server,wget \
-    $DISTRO $ROOTFS $MIRROR
+if [ ! -d $ROOTFS ] ; then
+    step "Running debootstrap"
+    $DEBOOTSTRAP_DIR/debootstrap \
+        --verbose --keyring=$KEYRING --arch=$ARCH \
+        $DISTRO $ROOTFS $MIRROR
+fi
 
-echo "Creating sources.list"
-python3 -m outscale_image_factory.create_sources_list  \
+step "Creating sources.list"
+python2 -m outscale_image_factory.create_sources_list  \
     $ROOTFS/etc/apt/sources.list $MIRROR $DISTRO
 
-echo "Installing packages"
-mount --bind /proc $ROOTFS/proc
+step "Repopulating /dev"
+# If we don't recreate the content of /dev, /dev/urandom does not exist and
+# installation of openssh-server fails
+mount -t proc none $ROOTFS/proc
+mount -t sysfs none $ROOTFS/sys
 chroot $ROOTFS apt-get update
+chroot $ROOTFS apt-get install makedev
+chroot $ROOTFS /sbin/MAKEDEV
+
+step "Installing packages"
+# This policy-rc.d file prevents daemons from starting on install/upgrade
+# See https://people.debian.org/~hmh/invokerc.d-policyrc.d-specification.txt
+POLICY_HELPER=$ROOTFS/usr/sbin/policy-rc.d
+echo 'exit 101' > $POLICY_HELPER
+chmod +x $POLICY_HELPER
+
 if [ -n "$(chroot $ROOTFS apt-cache search --names-only ^$DEBIAN_KERNEL$)" ]
 then
-    chroot $ROOTFS apt-get install -y $DEBIAN_KERNEL grub-pc
+    KERNEL_PACKAGE=$DEBIAN_KERNEL
 else
-    chroot $ROOTFS apt-get install -y $UBUNTU_KERNEL grub-pc
+    KERNEL_PACKAGE=$UBUNTU_KERNEL
 fi
-chroot $ROOTFS apt-get dist-upgrade -y
-umount $ROOTFS/proc
 
-echo "Copying overlay"
+chroot $ROOTFS apt-get install -y $KERNEL_PACKAGE $PACKAGES
+chroot $ROOTFS apt-get dist-upgrade -y
+
+rm $POLICY_HELPER
+umount $ROOTFS/proc
+umount $ROOTFS/sys
+
+step "Copying overlay"
 rsync -rlpDv $OVERLAY/ $ROOTFS/
 
-echo "Creating dummy files"
+step "Creating dummy files"
 touch $ROOTFS/usr/lib/inithooks/firstboot.d/30turnkey-init-fence
 cat <<EOF >$ROOTFS/etc/default/inithooks
 INITHOOKS_CONF=/etc/inithooks.conf
 EOF
 
-echo "Adding hooks to /etc/rc.local"
+step "Adding hooks to /etc/rc.local"
 cat <<EOF >$ROOTFS/etc/rc.local
 #!/bin/sh
 FIRSTBOOT=/usr/lib/inithooks/firstboot.d/99outscale
@@ -169,14 +232,14 @@ chmod +x \$EVERYBOOT
 \$EVERYBOOT
 EOF
 
-echo "Disabling root account"
+step "Disabling root account"
 chroot $ROOTFS passwd -d root
 chroot $ROOTFS passwd -l root
 
-echo "Cleaning up rootfs"
+step "Cleaning up rootfs"
 chroot $ROOTFS apt-get clean
 chroot $ROOTFS find /var/log -type f -exec rm {} +
 
-echo "Copying rootfs to device"
-python -m outscale_image_factory.main \
-    install-rootfs --device=$DEVICE --rootfs=$ROOTFS
+step "Copying rootfs to device"
+python2 -m outscale_image_factory.main \
+    install-rootfs --device=$DEVICE $ROOTFS
